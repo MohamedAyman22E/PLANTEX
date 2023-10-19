1baac40b5f0356f94df81d8a422f186ba74757db
moAymanEz <moAyman@gmail.com>
2023-10-19 18:19:23 +0300

    19-10-2023-2

--- js/action.js
+++ js/action.js
@@ -5,16 +5,44 @@ $(document).ready(function () {
   $(".fa-xmark").click(function () {
     $(".navList").removeClass("active");
   });
+  //
   $(".theme-icon").on("click", function () {
     $(".theme-icon").toggleClass("fa-sun");
     $(".theme-icon").toggleClass("fa-moon");
     $(":root").toggleClass("active");
   });
+  //
   $(document).scroll(function () {
     if ($(window).scrollTop() > 0) {
       $("header").addClass("sticky");
     } else {
       $("header").removeClass("sticky");
     }
+  });
+
+  //
+  $(".navList li a").click(function () {
+    $(this)
+      .addClass("active")
+      .parent()
+      .siblings()
+      .find("a")
+      .removeClass("active");
+    $("html,body").animation(
+      {
+        screenTop: $("#" + $(this).data("scroll")).offset().top + 5,
+      },
+      1000
+    );
+  });
+  //
+  $(window).scroll(function () {
+    $("section").each(function () {
+      if ($(window).scrollTop() > $(this).offset().top) {
+        var id = $(this).attr("id");
+        $(".navList a").removeClass("active");
+        $('.navList li a[data-scroll="' + id + '"]').addClass("active");
+      }
+    });
   });
 });
